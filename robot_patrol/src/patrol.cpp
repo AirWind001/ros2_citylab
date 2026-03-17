94cd61f18c662b4bf97fb9357a2d4942e1b1ec8d
AirWind001 <arvindraju98@gmail.com>
2026-03-17 16:23:40 +0000

    created Subcriber to LaserScan topic

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,14 +1,26 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription.hpp"
 
 class Patrol : public rclcpp::Node
 {
 public:
     Patrol() : Node("patrol_node")
     {
+        RCLCPP_INFO(this->get_logger(), "Patrol node has been started.");
+
+        laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/fastbot_1/scan", 
+        10, 
+        std::bind(&Patrol::laser_callback, this, std::placeholders::_1)
+        );
     }
 
 private:
-
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+    void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        float front = msg->ranges[0];
+        RCLCPP_INFO(this->get_logger(), "Front distance: %f", front);    
+    }
 };
 
 int main(int argc, char * argv[])
