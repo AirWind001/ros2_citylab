e0b75ceef0a6a33ef0774ac19e12182f2a6b0491
AirWind001 <arvindraju98@gmail.com>
2026-03-18 17:31:53 +0000

    added launch files to test service

---
+++ robot_patrol/launch/start_test_service.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='robot_patrol',
+            executable='test_service',
+            name='test_service_node',
+            output='screen'
+        )
+    ])
